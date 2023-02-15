37078b8052338823da70591f7e27f0cec99569c0
GoHakNeung <98439777+GoHakNeung@users.noreply.github.com>
2023-02-15 20:36:21 +0900

    Update code_check.py

--- code_check.py
+++ code_check.py
@@ -360,9 +360,9 @@ def update_excel(message, py) :
   worksheet.update_cell(row, col+1, trial_error_count[py])
 #------------------------------------------------------------------------------#
 #HTML 형식의 문제 불러오기기
-def Question(question_, img="") : 
-  display(HTML(question_))
-  return Image(url= img) 
+# def Question(question_, img="") : 
+#   display(HTML(question_))
+#   return Image(url= img) 
 #------------------------------------------------------------------------------#
 #코드의 정답 여부를 확인하는 함수
 def code_check(py) :
