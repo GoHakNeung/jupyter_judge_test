1fc18c3b8e349bb17055cae5a864dce3fe8042ff
GoHakNeung <98439777+GoHakNeung@users.noreply.github.com>
2024-02-23 22:28:19 +0900

    Create problem2.py
    온라인저지에서 활용되는 데이터로 
    도구  성능 검증에 활용

---
+++ problem2.py
@@ -0,0 +1,15 @@
+from IPython.display import display, HTML
+
+def Question(question_) : 
+  display(HTML(question_))
+
+question_0010 = '''
+코이스터디 0010번 문제
+'''
+img_1101 = "https://github.com/GoHakNeung/python/blob/main/img_1101.JPG?raw=true"
+answer_0010 = [{'input': [['-459']], 'output': ['-459']}, {'input': [['-33']], 'output': ['-33']}, {'input': [['-166']], 'output': ['-166']}, {'input': [['0']], 'output': ['0']}, {'input': [['-331']], 'output': ['-331']}, {'input': [['224']], 'output': ['224']}, {'input': [['-22']], 'output': ['-22']}, {'input': [['-142']], 'output': ['-142']}, {'input': [['462']], 'output': ['462']}, {'input': [['-36']], 'output': ['-36']}]
+
+test_set = [
+#입력,변수,출력_수와 연산산
+    {'test_file' : '0010', 'answer' : answer_0010},
+]
