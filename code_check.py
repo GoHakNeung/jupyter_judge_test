3f99d17372a60d198fc6803bddfbb5180385e3ed
GoHakNeung <98439777+GoHakNeung@users.noreply.github.com>
2024-08-26 10:06:20 +0900

    Update code_check.py

--- code_check.py
+++ code_check.py
@@ -67,6 +67,10 @@ def create_button_with_scratch_cell():
     # Python 측에서 호출할 함수를 등록
     def create_scratch_cell():
         global question_number, final_result, attempts, question_info
+        print(question_number)
+        print(final_result)
+        print(attempts)
+        print(question_info.head())
         next_question = recommend_next_question(question_number, final_result, question_info, attempts)
         _frontend.create_scratch_cell(f"#이 코드를 실행해주세요.\nQuestion('{next_question}')")
         # _frontend.create_scratch_cell(f'#이 코드를 실행해주세요.\nQuestion({question_file1})')
