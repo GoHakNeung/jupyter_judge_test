c3735492229495bc546194e5485057c5d206a74a
GoHakNeung <98439777+GoHakNeung@users.noreply.github.com>
2024-08-27 16:14:05 +0900

    Update problem.py

--- question_bank/problem.py
+++ question_bank/problem.py
@@ -10,7 +10,7 @@ def Question(question_number, _type = 'code'):
     globals_variable.total_attempts = 0
     
     question_path = '/content/jupyter_judge/question_bank/question/' + question_number+'.html'
-    question_name = 'question_'+question_number
+    globals_variable.question_name = 'question_'+question_number
     question_file = 'question_'+question_number
 
     file_name = '_'+ question_number+'.py'
@@ -21,13 +21,13 @@ def Question(question_number, _type = 'code'):
 
     code_editor = setup_html_js()
 
-    if question_name in globals() : 
-        display(HTML(globals()[question_name] + code_editor))
+    if globals_variable.question_name in globals() : 
+        display(HTML(globals()[globals_variable.question_name] + code_editor))
 
     else : 
         with open(question_path, 'r') as f :
             data = f.read()
-        globals()[question_name] = data
+        globals()[globals_variable.question_name] = data
 
         answer_path = '/content/jupyter_judge/question_bank/answer/answer_' + question_number + '.py'
         with open(answer_path, 'r') as f : 
@@ -40,7 +40,7 @@ def Question(question_number, _type = 'code'):
             meta = f.read()
         exec(meta)
   
-        display(HTML(globals()[question_name] + code_editor))
+        display(HTML(globals()[globals_variable.question_name] + code_editor))
 
     # 콜백 함수 등록
     def save_text(text):
